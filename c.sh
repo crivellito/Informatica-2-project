b125e5eae9b55e36e2ee2e5cbd4ac56db71ce798
crivellito <augusto.crivelli03@gmail.com>
2025-04-16 19:41:47 -0300

    Script c.sh actualizado con opcion de detectar si el nombre del archivo esta vacio

--- c.sh
+++ c.sh
@@ -4,6 +4,11 @@ echo "Creando programa en formato .c"
 
 read -p "Nombre del archivo?   " name
 
+if [ -z "$name" ]; then
+    echo "No se ha proporcionado un nombre. Saliendo..."
+    exit 1
+fi
+
 cat > "$name.c" <<EOL
 
 #include <stdio.h>
